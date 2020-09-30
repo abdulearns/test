9639520f98c1b4415dda9481941f2432c3804680
abdulearns <68906173+abdulearns@users.noreply.github.com>
2020-10-01 02:28:07 +0530

    Update tst.py
    Added exception handling

--- tst.py
+++ tst.py
@@ -8,9 +8,17 @@
 #     print('nadarimo')
 
 
-r = int(input('num'))
+###########################################################################
+# Program which takes a number as input and evaluates if it's even or odd #
+###########################################################################
 
-if r%2==0:
-    print('{} is even' .format(r))
+# Adding exception handling
+try:
+    r = int(input('Please enter a number: '))
+except:
+    print('Invalid input. Not a number..')
 else:
-    print('{} is fard',r)    
+    if r%2 == 0:
+        print('{} is even' .format(r))
+    else:
+        print('{} is fard' .format(r))
